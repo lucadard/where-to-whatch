611d7c49ed935ce733090d0642ccead78eb06d12
lucadard <lucadardenne@hotmail.com>
2022-09-14 15:28:19 -0300

    delete unused return in hook

--- hooks/useAutocompletion.jsx
+++ hooks/useAutocompletion.jsx
@@ -35,7 +35,6 @@ export const useAutocompletion = (query) => {
   }, [query])
 
   return {
-    autocompleteItems,
-    nextCompletion: autocompleteItems[0] || undefined
+    autocompleteItems
   }
 }
