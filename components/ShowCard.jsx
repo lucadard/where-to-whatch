db77a6d7728f88a554daff8b344aaf0b31da5e5a
lucadard <lucadardenne@hotmail.com>
2022-09-14 15:29:09 -0300

    change name of services for providers and add loading state

--- components/ShowCard.jsx
+++ components/ShowCard.jsx
@@ -3,9 +3,11 @@ import axios from 'axios'
 import Image from 'next/image'
 
 const ShowCard = ({ show }) => {
-  const [services, setServices] = useState([])
+  const [providers, setProviders] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    setLoading(true)
     const fetchData = async () => {
       const { data } = await axios({
         method: 'get',
@@ -18,27 +20,44 @@ const ShowCard = ({ show }) => {
       const { AR } = data.results
       return AR ? data.results.AR.flatrate : []
     }
-    fetchData().then((res) => setServices(res))
+    fetchData().then((res) => {
+      setProviders(res)
+      setLoading(false)
+    })
   }, [show])
 
   return (
-    <div>
-      <h3>Where to stream:</h3>
-      <div className="providers">
-        {services.map((service) => (
-          <div key={service.provider_id}>
-            {/* <p>{service.provider_name}</p> */}
-            <div className="image-container">
-              <Image
-                src={`https://www.themoviedb.org/t/p/original${service.logo_path}`}
-                alt={service.provider_name + ' logo'}
-                layout="fill"
-              />
-            </div>
+    <div className="container">
+      {loading ? (
+        <span>loading...</span>
+      ) : (
+        <>
+          <h3>Where to stream:</h3>
+          <div className="providers">
+            {providers.length ? (
+              providers.map((provider) => (
+                <div key={provider.provider_id}>
+                  {/* <p>{service.provider_name}</p> */}
+                  <div className="image-container">
+                    <Image
+                      src={`https://www.themoviedb.org/t/p/original${provider.logo_path}`}
+                      alt={provider.provider_name + ' logo'}
+                      layout="fill"
+                    />
+                  </div>
+                </div>
+              ))
+            ) : (
+              <span>No data available.</span>
+            )}
           </div>
-        ))}
-      </div>
+        </>
+      )}
+
       <style jsx>{`
+        .container {
+          margin: 0 3rem;
+        }
         .providers {
           display: flex;
           gap: 1rem;
