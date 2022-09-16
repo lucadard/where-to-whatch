c3c37ce3e385fb2b3c5f89d1d5bd839dac81e58c
lucadard <lucadardenne@hotmail.com>
2022-09-16 13:14:11 -0300

    add loading skeleton

--- components/Autocomplete.jsx
+++ components/Autocomplete.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef } from 'react'
 
 import { useAutocompletion } from '../hooks/useAutocompletion'
 import { useKeys } from '../hooks/useKeys'
-import { AutocompleteTitleSkeleton } from '../assets/Skeleton'
 
 const Autocomplete = ({ query, setQuery, setCompletion, show }) => {
   const { autocompleteItems, loading } = useAutocompletion(query)
@@ -29,36 +28,33 @@ const Autocomplete = ({ query, setQuery, setCompletion, show }) => {
 
   return (
     <ul
-      className={`bg-gray-700 mt-11 rounded-b-3xl overflow-hidden absolute w-full h-[253px] py-1 ${
+      className={`pt-[1px] pb-2 bg-white border-gray-300 border-t-0 border-[1px] shadow-md mt-11 rounded-b-3xl overflow-hidden absolute left-0 right-0 ${
         show && !query.id && query.text ? '' : 'hidden'
       }`}
       onMouseLeave={() => setSelectedItem()}
     >
-      <div className="border-gray-500 border-t-[1px] w-11/12 m-auto pb-2"></div>
-      {loading
-        ? [0, 1, 2, 3, 4].map((_, index) => (
-            <li className="h-12 flex px-6 items-center" key={index}>
-              <div className="h-4 w-full bg-white opacity-80 rounded animate-pulse -mt-1" />
-            </li>
-          ))
-        : autocompleteItems.map((item, index) => (
-            <li
-              key={item.id}
-              className={`p-3 px-6 cursor-default overflow-hidden ${
-                selectedItem === index ? 'bg-gray-600' : ''
-              }`}
-              onMouseEnter={() => setSelectedItem(index)}
-            >
-              <p
-                onClick={() =>
-                  setQuery({ ...query, id: item.id, text: item.title })
-                }
-                className="font-semibold whitespace-nowrap text-ellipsis"
-              >
-                {item.title}
-              </p>
-            </li>
-          ))}
+      <div className="border-gray-300 border-t-[1px] w-11/12 m-auto pb-2" />
+
+      {autocompleteItems.map((item, index) => (
+        <li
+          key={index}
+          className={`py-3 px-6 cursor-default h-12 flex items-center ${
+            selectedItem === index ? 'bg-black bg-opacity-10' : ''
+          }`}
+          onMouseEnter={() => setSelectedItem(index)}
+          onClick={() =>
+            !loading && setQuery({ ...query, id: item.id, text: item.title })
+          }
+        >
+          {loading ? (
+            <span className="h-4 w-full opacity-80 rounded animate-pulse bg-gray-300" />
+          ) : (
+            <p className="whitespace-nowrap text-ellipsis overflow-hidden">
+              {item.title}
+            </p>
+          )}
+        </li>
+      ))}
     </ul>
   )
 }
