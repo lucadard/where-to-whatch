752f01b4c5d663f40b893f3b471d69dcebf9a690
lucadard <lucadardenne@hotmail.com>
2022-09-16 00:32:22 -0300

    add hook to detect when a component is clicked on

---
+++ hooks/useComponentFocus.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+
+export const useComponentFocus = (ref) => {
+  const [focus, setFocus] = useState(false)
+
+  useEffect(() => {
+    const handleClickOutside = (event) =>
+      setFocus(() => {
+        let condition = ref.current && !ref.current.contains(event.target)
+        console.log(condition ? 'click out!' : 'click in!')
+        return condition ? false : true
+      })
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => document.removeEventListener('mousedown', handleClickOutside)
+  }, [ref])
+
+  return {
+    focus
+  }
+}
