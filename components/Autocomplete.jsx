4d96fb2494f70e888729d1e12aac0b0c63b93195
lucadard <lucadardenne@hotmail.com>
2022-09-20 16:31:49 -0300

    delete keys implementation

--- components/Autocomplete.jsx
+++ components/Autocomplete.jsx
@@ -1,56 +1,41 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState } from 'react'
 
 import { useAutocompletion } from '../hooks/useAutocompletion'
 import { useKeys } from '../hooks/useKeys'
 
-const Autocomplete = ({ query, setQuery, setCompletion, show }) => {
-  const { autocompleteItems, loading } = useAutocompletion(query)
+const Autocomplete = ({ input, show, focus, unhideShow }) => {
+  const { autocompleteItems, loading } = useAutocompletion(
+    input.get,
+    show.get.type
+  )
   const [selectedItem, setSelectedItem] = useState(undefined)
-  const { keys } = useKeys()
 
-  useEffect(() => {
-    let autocompleteLenght = autocompleteItems.length - 1
-    if (keys.up)
-      setSelectedItem((prevItem) => {
-        if (prevItem === undefined) return autocompleteLenght
-        return prevItem === 0 ? autocompleteLenght : prevItem - 1
-      })
-    else if (keys.down)
-      setSelectedItem((prevItem) => {
-        if (prevItem === undefined) return 0
-        return prevItem === autocompleteLenght ? 0 : prevItem + 1
-      })
-  }, [keys, autocompleteItems])
-
-  useEffect(() => {
-    setCompletion(autocompleteItems[selectedItem])
-  }, [setCompletion, autocompleteItems, selectedItem])
+  const handleSelectShow = (newShow) => {
+    show.set((prev) => ({ ...prev, id: newShow.id, title: newShow.title }))
+    input.set(newShow.title)
+    unhideShow()
+  }
 
   return (
     <ul
-      className={`pt-[1px] mx-4 pb-2 bg-white border-gray-300 border-t-0 border-[1px] shadow-md mt-11 rounded-b-3xl overflow-hidden absolute left-0 right-0 ${
-        show && !query.id && query.text ? '' : 'hidden'
-      }`}
-      onMouseLeave={() => setSelectedItem()}
+      className={`pt-[2px] min-h-[30px] pb-2 bg-white shadow-md mt-11 rounded-b-3xl absolute w-full ring-[1px] ring-gray-300 
+      ${!focus || !input.get || show.get.id ? 'hidden' : ''}`}
     >
-      <div className="border-gray-300 border-t-[1px] w-11/12 m-auto pb-2" />
+      <div className="bg-white w-full h-2 -mt-1">
+        <div className="h-[1px] w-11/12 bg-gray-300 mx-auto" />
+      </div>
 
       {autocompleteItems.map((item, index) => (
         <li
           key={index}
-          className={`py-3 px-6 cursor-default h-12 flex items-center ${
-            selectedItem === index ? 'bg-black bg-opacity-10' : ''
-          }`}
-          onMouseEnter={() => setSelectedItem(index)}
-          onClick={() =>
-            !loading && setQuery({ ...query, id: item.id, text: item.title })
-          }
+          className={`py-3 px-6 cursor-default h-12 flex items-center hover:bg-black hover:bg-opacity-10`}
+          onClick={() => !loading && handleSelectShow(item)}
         >
           {loading ? (
             <span className="h-4 w-full opacity-80 rounded animate-pulse bg-gray-300" />
           ) : (
             <p className="whitespace-nowrap text-ellipsis overflow-hidden">
-              {item.title}
+              {item.title} <span>({item.year})</span>
             </p>
           )}
         </li>
