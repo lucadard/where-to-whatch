4971938e60c5b41dcd8fbc0b57414fad64f7052a
lucadard <lucadardenne@hotmail.com>
2022-09-20 16:33:20 -0300

    add popularity filter and release date

--- hooks/useAutocompletion.jsx
+++ hooks/useAutocompletion.jsx
@@ -2,42 +2,66 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const URL = 'https://api.themoviedb.org/3/search/'
+const LENGTH = 5
 
-export const useAutocompletion = (query) => {
-  const [autocompleteItems, setAutocompleteItems] = useState(new Array(5))
+export const useAutocompletion = (input, type) => {
+  const [autocompleteItems, setAutocompleteItems] = useState(new Array(LENGTH))
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     setLoading(true)
-    if (query.text === '' || query.id) return setAutocompleteItems([])
+    if (!input) return setAutocompleteItems([])
     const fetchAutocomplete = async () => {
       const { data } = await axios({
         method: 'get',
-        url: URL + query.type,
+        url: URL + type,
         params: {
           api_key: process.env.API_KEY,
-          query: query.text
+          query: input
         }
       })
-      return data.results.slice(0, 5).reduce((array, item) => {
-        array.push({
-          id: item.id,
-          title: query.type === 'tv' ? item.original_name : item.original_title
-        })
-        return array
-      }, [])
+
+      const results = data.results
+        .slice(0, 5)
+        .sort((a, b) => b.popularity - a.popularity)
+
+      if (type === 'tv')
+        return results.reduce((array, item) => {
+          array.push({
+            id: item.id,
+            title: item.name,
+            year:
+              item.first_air_date === ''
+                ? 'not aired yet'
+                : item.first_air_date.slice(0, item.first_air_date.indexOf('-'))
+          })
+          return array
+        }, [])
+      else
+        return results.reduce((array, item) => {
+          array.push({
+            id: item.id,
+            title: item.title,
+            year:
+              item.release_date === ''
+                ? 'not aired yet'
+                : item.release_date.slice(0, item.release_date.indexOf('-'))
+          })
+          return array
+        }, [])
     }
     const timeOutId = setTimeout(() => {
-      fetchAutocomplete(query.text).then((res) => {
+      fetchAutocomplete(input).then((res) => {
         setAutocompleteItems(res)
         setLoading(false)
       })
     }, 1000)
     return () => clearTimeout(timeOutId)
-  }, [query])
+  }, [input, type])
 
   return {
     autocompleteItems,
-    loading
+    loading,
+    length: autocompleteItems.length
   }
 }
