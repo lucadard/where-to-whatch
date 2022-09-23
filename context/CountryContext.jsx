c1817217857f697e100e2307440180051c717bab
lucadard <lucadardenne@hotmail.com>
2022-09-23 13:57:23 -0300

    add functionality to fetch data from url

--- context/CountryContext.jsx
+++ context/CountryContext.jsx
@@ -1,10 +1,35 @@
+import { useRouter } from 'next/router'
 import { createContext, useContext, useState } from 'react'
 
 const CountryContext = createContext(undefined)
 
+const countries = [
+  { name: 'argentina', code: 'AR' },
+  { name: 'united states', code: 'US' },
+  { name: 'spain', code: 'ES' },
+  { name: 'brazil', code: 'BR' },
+  { name: 'uruguay', code: 'UY' },
+  { name: 'chile', code: 'CL' },
+  { name: 'japan', code: 'JP' }
+]
+
+const defaultCountry = { name: 'argentina', code: 'AR' }
+
 const CountryProvider = ({ children }) => {
-  const [selectedCountry, setSelectedCountry] = useState('AR')
-  const value = { selectedCountry, setSelectedCountry }
+  const router = useRouter()
+
+  const [country, setCountry] = useState(
+    () =>
+      countries.find((country) => router.query.lang === country.code) ||
+      defaultCountry
+  )
+
+  const value = {
+    countries,
+    country,
+    setCountry
+  }
+
   return (
     <CountryContext.Provider value={value}>{children}</CountryContext.Provider>
   )
