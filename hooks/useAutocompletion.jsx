aece9bf00c5c6c33a9762588a587cec567f30c1c
lucadard <lucadardenne@hotmail.com>
2022-09-23 13:55:52 -0300

    now movies and tv requests are unified

--- hooks/useAutocompletion.jsx
+++ hooks/useAutocompletion.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 const URL = 'https://api.themoviedb.org/3/search/'
 const LENGTH = 5
 
-export const useAutocompletion = (input, type) => {
+export const useAutocompletion = (input) => {
   const [autocompleteItems, setAutocompleteItems] = useState(new Array(LENGTH))
   const [loading, setLoading] = useState(true)
 
@@ -12,43 +12,56 @@ export const useAutocompletion = (input, type) => {
     setLoading(true)
     if (!input) return setAutocompleteItems([])
     const fetchAutocomplete = async () => {
-      const { data } = await axios({
+      const { data: dataTv } = await axios({
         method: 'get',
-        url: URL + type,
+        url: URL + 'tv',
+        params: {
+          api_key: process.env.API_KEY,
+          query: input
+        }
+      })
+      const { data: dataMovies } = await axios({
+        method: 'get',
+        url: URL + 'movie',
         params: {
           api_key: process.env.API_KEY,
           query: input
         }
       })
 
-      const results = data.results
-        .slice(0, 5)
-        .sort((a, b) => b.popularity - a.popularity)
+      const resultsTv = dataTv.results.slice(0, 5).reduce((array, item) => {
+        array.push({
+          id: item.id,
+          title: item.name,
+          year: !item.first_air_date
+            ? 'not aired yet'
+            : item.first_air_date.slice(0, item.first_air_date.indexOf('-')),
+          type: 'tv',
+          popularity: item.popularity
+        })
+        return array
+      }, [])
 
-      if (type === 'tv')
-        return results.reduce((array, item) => {
-          array.push({
-            id: item.id,
-            title: item.name,
-            year:
-              item.first_air_date === ''
-                ? 'not aired yet'
-                : item.first_air_date.slice(0, item.first_air_date.indexOf('-'))
-          })
-          return array
-        }, [])
-      else
-        return results.reduce((array, item) => {
+      const resultsMovie = dataMovies.results
+        .slice(0, 5)
+        .reduce((array, item) => {
           array.push({
             id: item.id,
             title: item.title,
-            year:
-              item.release_date === ''
-                ? 'not aired yet'
-                : item.release_date.slice(0, item.release_date.indexOf('-'))
+            year: !item.release_date
+              ? 'not aired yet'
+              : item.release_date.slice(0, item.release_date.indexOf('-')),
+            type: 'movie',
+            popularity: item.popularity
           })
           return array
         }, [])
+
+      const results = resultsTv
+        .concat(resultsMovie)
+        .sort((a, b) => b.popularity - a.popularity)
+
+      return results
     }
     const timeOutId = setTimeout(() => {
       fetchAutocomplete(input).then((res) => {
@@ -57,7 +70,7 @@ export const useAutocompletion = (input, type) => {
       })
     }, 1000)
     return () => clearTimeout(timeOutId)
-  }, [input, type])
+  }, [input])
 
   return {
     autocompleteItems,
