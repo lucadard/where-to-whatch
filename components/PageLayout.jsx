e8559b98553082f12d861ec6ee22dad911fe26f9
lucadard <lucadardenne@hotmail.com>
2022-11-18 19:19:53 -0300

    add useTitle and useTheme functionality

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import Head from 'next/head'
+import useTitle from '../hooks/useTitle'
+import { useTheme } from '../context/ThemeContext'
 
-const PageLayout = ({ children, title }) => {
+const PageLayout = ({ children }) => {
+  const { title } = useTitle()
+  const { theme } = useTheme()
   return (
     <>
       <Head>
         <title>{title}</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <main className="h-screen">{children}</main>
+      <main
+        className={`h-screen
+        ${theme === 'light' ? 'bg-light text-dark' : 'bg-dark text-light'}`}
+      >
+        {children}
+      </main>
     </>
   )
 }
