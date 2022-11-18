52dc83463c3c8889f33e64620c67aa4d497fef89
lucadard <lucadardenne@hotmail.com>
2022-11-18 19:08:13 -0300

    add Title and ThemeSelector

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
 
 import PageLayout from '../components/PageLayout'
+import ThemeSelector from '../components/ThemeSelector'
+import Title from '../components/Title'
 import Input from '../components/Input'
 import ShowCard from '../components/ShowCard'
 
 import { useShow } from '../context/ShowContext'
 
-export default function Show() {
+export default function App() {
   const { show } = useShow()
 
   return (
     <PageLayout title="Where to watch?">
-      <div className="flex flex-col items-center gap-6 mx-3 md:mx-0 mt-24">
+      <div className="flex flex-col items-center pt-10 md:pb-10 gap-6 transition-colors duration-300">
+        <Title />
         <Input />
         {show.id !== 'search' && <ShowCard />}
+        <ThemeSelector />
       </div>
     </PageLayout>
   )
