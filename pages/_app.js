b79d65610e2a1f424ef338317872cd4553d4e16b
lucadard <lucadardenne@hotmail.com>
2022-11-18 19:07:51 -0300

    add ThemeProvider and getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,22 @@
+import { getCookie } from 'cookies-next'
+
+import { ThemeProvider } from '../context/ThemeContext'
 import { CountryProvider } from '../context/CountryContext'
 import { ShowProvider } from '../context/ShowContext'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, theme }) {
   return (
-    <CountryProvider>
-      <ShowProvider>
-        <Component {...pageProps} />
-      </ShowProvider>
-    </CountryProvider>
+    <ThemeProvider loadedTheme={theme}>
+      <CountryProvider>
+        <ShowProvider>
+          <Component {...pageProps} />
+        </ShowProvider>
+      </CountryProvider>
+    </ThemeProvider>
   )
 }
 
 export default MyApp
+
+MyApp.getInitialProps = ({ ctx }) => ({ theme: getCookie('theme', ctx) })
