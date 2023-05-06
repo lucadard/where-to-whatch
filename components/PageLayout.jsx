06308b52ec10c7dd96266b7b59ae8795311edd2b
lucadard <lucadardenne@hotmail.com>
2023-05-06 19:52:44 -0300

    add footer

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import useTitle from '../hooks/useTitle'
 import { useTheme } from '../context/ThemeContext'
+import Link from 'next/link'
 
 const PageLayout = ({ children }) => {
   const { title } = useTitle()
@@ -20,11 +21,21 @@ const PageLayout = ({ children }) => {
         <title>{title}</title>
         <link rel='icon' href='/favicon.png' />
       </Head>
-      <main
-        className='min-h-screen pb-10'
-      >
+      <main className='min-h-screen flex flex-col mx-auto justify-between pt-14'>
         {children}
       </main>
+      <footer className='bg-black/10 border-t border-black/10 py-3'>
+        <p className='text-center'>
+          Made by{' '}
+          <a
+            className='text-blue-700'
+            href='https://github.com/lucadard'
+            target='_blank'
+            rel='noreferrer'
+          >lucadard
+          </a>
+        </p>
+      </footer>
     </>
   )
 }
