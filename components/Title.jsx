c8effbee18e8fe7bbc118922bd20584c4689694c
lucadard <lucadardenne@hotmail.com>
2023-05-06 18:43:37 -0300

    apply linter fixes

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import React from 'react'
 
-function Title() {
+function Title () {
   return (
-    <div className="w-full max-w-[600px] px-3">
-      <h1 className="text-4xl font-bold">
-        <a href="/">Where to watch?</a>
+    <div className='w-full max-w-[600px] px-3'>
+      <h1 className='text-4xl font-bold'>
+        <a href='/'>Where to watch?</a>
       </h1>
-      <span className="italic font-thin">
+      <span className='italic font-thin'>
         Find where your favourite show is being streamed.
       </span>
     </div>
